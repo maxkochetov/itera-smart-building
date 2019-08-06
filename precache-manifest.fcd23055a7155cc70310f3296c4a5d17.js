7ae6e21dd2f52d4d20faec69f62de2405e8f3cfe
Maksym Kochetov <maksym.kochetov@ite-mac-01-301.local>
2019-08-06 10:39:14 +0200

    Updates

---
+++ precache-manifest.fcd23055a7155cc70310f3296c4a5d17.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745d454904f24350cf44c264cc0ed952",
+    "url": "/itera-smart-building/index.html"
+  },
+  {
+    "revision": "e1b0f78b1b20aeebae2d",
+    "url": "/itera-smart-building/static/css/3.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ca9786dcc772af83c416",
+    "url": "/itera-smart-building/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e1b0f78b1b20aeebae2d",
+    "url": "/itera-smart-building/static/js/3.6ec750d3.chunk.js"
+  },
+  {
+    "revision": "b847727bd5638647ace0",
+    "url": "/itera-smart-building/static/js/4.83b2610e.chunk.js"
+  },
+  {
+    "revision": "71dcb9e73f09892b23ce",
+    "url": "/itera-smart-building/static/js/5.c3f7a0a2.chunk.js"
+  },
+  {
+    "revision": "fcdfc1e6d6868b1b293c",
+    "url": "/itera-smart-building/static/js/6.7657373e.chunk.js"
+  },
+  {
+    "revision": "ca9786dcc772af83c416",
+    "url": "/itera-smart-building/static/js/main.649688f0.chunk.js"
+  },
+  {
+    "revision": "51688e4e531f0533b7fe",
+    "url": "/itera-smart-building/static/js/runtime~main.9dcdb811.js"
+  },
+  {
+    "revision": "c1ee37220b5607c75ccb",
+    "url": "/itera-smart-building/static/js/xlsx.f50fc01d.chunk.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed2ba0531a39254f04189c2efa770d52.js"
+  "/itera-smart-building/precache-manifest.fcd23055a7155cc70310f3296c4a5d17.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/itera-smart-building/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
