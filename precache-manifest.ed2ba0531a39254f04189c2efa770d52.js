f9a17166327f02d049a0b91e9be0ff6173aef35d
Maksym Kochetov <maksym.kochetov@ite-mac-01-301.local>
2019-08-06 10:43:29 +0200

    Updates

---
+++ precache-manifest.ed2ba0531a39254f04189c2efa770d52.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca6e87990187599f43b62d5a517ec3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b0f78b1b20aeebae2d",
+    "url": "/static/css/3.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9f807bdf56bee8a0686f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e1b0f78b1b20aeebae2d",
+    "url": "/static/js/3.6ec750d3.chunk.js"
+  },
+  {
+    "revision": "b847727bd5638647ace0",
+    "url": "/static/js/4.83b2610e.chunk.js"
+  },
+  {
+    "revision": "71dcb9e73f09892b23ce",
+    "url": "/static/js/5.c3f7a0a2.chunk.js"
+  },
+  {
+    "revision": "fcdfc1e6d6868b1b293c",
+    "url": "/static/js/6.7657373e.chunk.js"
+  },
+  {
+    "revision": "9f807bdf56bee8a0686f",
+    "url": "/static/js/main.2c4ea30c.chunk.js"
+  },
+  {
+    "revision": "1808fceeebb237e2a70a",
+    "url": "/static/js/runtime~main.bf87fa28.js"
+  },
+  {
+    "revision": "c1ee37220b5607c75ccb",
+    "url": "/static/js/xlsx.f50fc01d.chunk.js"
+  }
+]);
